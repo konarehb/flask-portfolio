d50b9244a1c1850390428b609db3327d32fd9e2e
konarehb <konarehb@gmail.com>
2024-07-08 21:53:46 +0000

    Ajouter la section contact et le footer

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/templates/index')
 def index():
-    return render_template('index.html', title='')
+    current_year = datetime.datetime.now().year
+    return render_template('index.html', current_year=current_year)
 
 if __name__ == '__main__':
     app.run(debug=True)
